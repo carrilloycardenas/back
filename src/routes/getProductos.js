f3fbd30249c1bb21968b103ac3f04499f47d8f4c
Daniel Cárdenas <daniel.cardenas112957@gmail.com>
2023-03-27 17:11:50 -0700

    query?

--- src/routes/getProductos.js
+++ src/routes/getProductos.js
@@ -1,17 +1,26 @@
 const { Router } = require('express');
 const { json } = require('sequelize');
-const { models } = require('../db/index');
+const { models, sequelize } = require('../db/index');
+const { Sequelize } = require ("sequelize");
 const router = Router();
+
 
 //Raiz
 router.get("/", async (req,res) => {
     try{
-        let LstProductos = await models.Productos.findAll();
-        res.json(LstProductos);
+        let LstProductos = await sequelize.query('SELECT * FROM Productos');
+        if(LstProductos != null){
+            res.json(LstProductos);
+        }else{
+            res.status(404).json({
+                msg: 'no se encontro el articulo'
+            })
+        }
 
     }catch(error){
         console.log(error);
     }
 });
+
 
 module.exports = router;
